e11e7c6a22b366b7d87120956b774d4e93f814dc
AdvellerdC <84400051+AdvellerdC@users.noreply.github.com>
2021-09-16 19:54:02 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace webreq
+{
+    public class Webreq {
+        public void webrequest(string website1) {	
+            WebRequest request = WebRequest.Create (website1);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine (response.StatusDescription);
+            Stream dataStream = response.GetResponseStream ();
+            StreamReader reader = new StreamReader (dataStream);
+            string responseFromServer = reader.ReadToEnd ();
+            Console.WriteLine (responseFromServer);
+            reader.Close ();
+            dataStream.Close ();
+            response.Close ();
+        }
+    }
+    public class WebRequestGetExample
+    {
+        
+        public static void Main()
+        {
+            string website = Console.ReadLine();
+            Webreq c = new Webreq();
+            c.webrequest(website);
+        }
+    }
+}
